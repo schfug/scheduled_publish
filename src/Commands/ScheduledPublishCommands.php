ceea44dcfaab843f30c4682121e2f9e49c3680da
Sascha Hannes <hannes@publicplan.de>
2018-08-20 20:48:39 +0200

    Used service, added ultimate cron as soft dependency

---
+++ src/Commands/ScheduledPublishCommands.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\scheduled_publish\Commands;
+
+use Drupal\scheduled_publish\Service\ScheduledPublishCron;
+use Drush\Commands\DrushCommands;
+
+class ScheduledPublishCommands extends DrushCommands {
+
+  /**
+   * @var \Drupal\scheduled_publish\Service\ScheduledPublishCron
+   */
+  private $publishCron;
+
+  /**
+   * ScheduledPublishCommands constructor.
+   *
+   * @param \Drupal\scheduled_publish\Service\ScheduledPublishCron $publishCron
+   */
+  public function __construct(ScheduledPublishCron $publishCron) {
+    $this->publishCron = $publishCron;
+  }
+
+  /**
+   *
+   *
+   * @command scheduled_publish:doUpdate
+   * @aliases schp
+   *
+   */
+  public function doUpdate() {
+    $this->cron->doUpdate();
+  }
+}
